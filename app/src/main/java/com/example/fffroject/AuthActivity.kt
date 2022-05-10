ba8150e2632f099255f0b56a4403107f3a8d3156
Minyoung-Lee <leemy08@naver.com>
2022-05-10 13:15:08 +0900

    [ADD] FireStore user정보 업로드

--- app/src/main/java/com/example/fffroject/AuthActivity.kt
+++ app/src/main/java/com/example/fffroject/AuthActivity.kt
@@ -10,14 +10,27 @@ import com.example.fffroject.databinding.ActivityAuthBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
     lateinit var binding: ActivityAuthBinding
+
+    var auth : FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 파이어스토어를 위한 Gradle 추가 및 import
+        // 인스턴스 추가시 오류 없다면 잘 된 것
+        // 파이어스토어 인스턴스 초기화
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         if(FFFroject.checkAuth()){
             changeVisibility("login")
@@ -39,56 +52,66 @@ class AuthActivity : AppCompatActivity() {
                         if (task.isSuccessful) {
                             // 구글 로그인 성공
                             FFFroject.email = account.email
-                            changeVisibility("login")
-                        } else {
+
+                            // 민영 추가(파이어스토어 연동 시도)
+                            var user = auth!!.currentUser
+
+                            if(user != null) {
+                                Toast.makeText(baseContext, user?.uid + user?.email,
+                                    Toast.LENGTH_SHORT).show()
+                                firestore?.collection("user")?.document(user.uid)
+                                    ?.set(hashMapOf("email" to user?.email, "userid" to user?.uid))
+                            }
+                                changeVisibility("login")
+                            } else {
                             // 구글 로그인 실패
                             changeVisibility("logout")
+                            }
                         }
-                    }
-            } catch (e: ApiException) {
+                } catch (e: ApiException) {
                 changeVisibility("logout")
             }
         }
 
-        binding.btnLogout.setOnClickListener {
-            // 구글 계정 로그아웃
-            Toast.makeText(baseContext, "로그아웃 누름",
-                Toast.LENGTH_SHORT).show()
-            FFFroject.auth.signOut()
-            FFFroject.email = null
-            changeVisibility("logout")
+            binding.btnLogout.setOnClickListener {
+                // 구글 계정 로그아웃
+                Toast.makeText(baseContext, "로그아웃 누름",
+                    Toast.LENGTH_SHORT).show()
+                FFFroject.auth.signOut()
+                FFFroject.email = null
+                changeVisibility("logout")
+            }
+
+            binding.btnLoginGoogle.setOnClickListener {
+                // 구글 로그인
+                val loginGoogle = GoogleSignInOptions
+                    .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.google_client_id))   // default_web_client_id 인데 오류발생(이미사용되고있어서)
+                    // strings.xml에서 새로 선언 후 json의 oatuh에서 client id 옆부분을 넣어서 바꿔줌 -> 오류 안남
+                    .requestEmail()
+                    .build()
+                // 구글의 인증 관리 앱 실행
+                val signInIntent = GoogleSignIn.getClient(this, loginGoogle).signInIntent
+                requestLauncher.launch(signInIntent)
+            }
+
         }
 
-        binding.btnLoginGoogle.setOnClickListener {
-            // 구글 로그인
-            val loginGoogle = GoogleSignInOptions
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.google_client_id))   // default_web_client_id 인데 오류발생(이미사용되고있어서)
-                // strings.xml에서 새로 선언 후 json의 oatuh에서 client id 옆부분을 넣어서 바꿔줌 -> 오류 안남
-                .requestEmail()
-                .build()
-            // 구글의 인증 관리 앱 실행
-            val signInIntent = GoogleSignIn.getClient(this, loginGoogle).signInIntent
-            requestLauncher.launch(signInIntent)
-        }
+        fun changeVisibility(mode: String){
+            if(mode === "login") {
+                binding.run {
+                    texLoginCheck.text = "${FFFroject.email} 님 안녕하세요!"
+                    btnLogout.visibility = View.VISIBLE
+                    btnLoginGoogle.visibility = View.GONE
+                }
+            }
 
-    }
-
-    fun changeVisibility(mode: String){
-        if(mode === "login") {
-            binding.run {
-                texLoginCheck.text = "${FFFroject.email} 님 안녕하세요!"
-                btnLogout.visibility = View.VISIBLE
-                btnLoginGoogle.visibility = View.GONE
+            else if(mode === "logout"){
+                binding.run {
+                    texLoginCheck.text = "로그인 해 주세요!"
+                    btnLogout.visibility = View.GONE
+                    btnLoginGoogle.visibility = View.VISIBLE
+                }
             }
         }
-
-        else if(mode === "logout"){
-            binding.run {
-                texLoginCheck.text = "로그인 해 주세요!"
-                btnLogout.visibility = View.GONE
-                btnLoginGoogle.visibility = View.VISIBLE
-            }
-        }
     }
-}
