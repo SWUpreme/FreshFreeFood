4031b553e8acdda41d4fa899ab13ea7061ebf28a
Minyoung-Lee <leemy08@naver.com>
2022-05-20 02:30:10 +0900

    [ADD] 나눔 버튼 액티비티 연동

--- app/src/main/java/com/example/fffroject/fragment/ShareFragment.kt
+++ app/src/main/java/com/example/fffroject/fragment/ShareFragment.kt
@@ -1,24 +1,50 @@
 package com.example.fffroject.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.fffroject.AuthActivity
 import com.example.fffroject.R
+import com.example.fffroject.SharePostActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ShareFragment : Fragment() {
-    fun newInstance() : ShareFragment {
-        return ShareFragment()
-    }
+    var auth: FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
+    var user: FirebaseUser? = null
 
+    lateinit var btn_addShare: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_share, container, false)
 
-        return inflater.inflate(R.layout.fragment_share, container, false)
+        // 파이어베이스 인증 객체
+        auth = FirebaseAuth.getInstance()
+        user = auth!!.currentUser
+        // 파이어스토어 인스턴스 초기화
+        firestore = FirebaseFirestore.getInstance()
+
+        btn_addShare = view.findViewById(R.id.btnShareAdd)
+
+        // 나눔 게시글 추가(게시글 추가 액티비티로 이동)
+        btn_addShare?.setOnClickListener {
+            val intent = Intent(activity, SharePostActivity::class.java)
+            startActivity(intent)
+            //activity?.let { ContextCompat.startActivity(it, intent, null) }
+        }
+
+
+        return view
     }
 }
