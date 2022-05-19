6f44059bb6ef393233e9217c19ed12f0c224a4a1
Minyoung-Lee <leemy08@naver.com>
2022-05-20 01:10:47 +0900

    [ADD] FoodList activity, xml 추가 SharePost activity, xml 추가

---
+++ app/src/main/java/com/example/fffroject/FoodListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fffroject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FoodListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_foodlist)
+    }
+}
---
+++ app/src/main/java/com/example/fffroject/SharePostActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fffroject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SharePostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_sharepost)
+    }
+}
