eeab94b5b335690839517744fc06b7ad27d3b871
serak0310 <serak0310@naver.com>
2022-05-31 04:34:49 +0900

    [ADD] 무료나눔 리사이클러뷰 구성

--- app/src/main/java/com/example/fffroject/fragment/ShareFragment.kt
+++ app/src/main/java/com/example/fffroject/fragment/ShareFragment.kt
@@ -8,11 +8,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fffroject.AuthActivity
@@ -33,8 +32,7 @@ class ShareFragment : Fragment() {
     var db: FirebaseFirestore? = null
     var user: FirebaseUser? = null
 
-    // 바인딩 객체
-    //lateinit var binding: FragmentShareBinding
+    // 바인딩
     lateinit var btnShareAdd: ImageButton
     lateinit var recyclerviewShare: RecyclerView
 
@@ -57,23 +55,21 @@ class ShareFragment : Fragment() {
         // 파이어베이스 인스턴스 초기화
         db = FirebaseFirestore.getInstance()
 
-        // 바인딩 객체 획득
-        //binding = FragmentShareBinding.inflate(layoutInflater)
-
         // 바인딩
         btnShareAdd= view.findViewById(R.id.btnShareAdd)
         recyclerviewShare= view.findViewById(R.id.recyclerviewShare)
 
-
-
-        /*11111
         // 파이어베이스에서 냉장고 값 불러오기
         loadData()
 
-        binding.recyclerviewShare.layoutManager = LinearLayoutManager(activity)
-        binding.recyclerviewShare.adapter=ShareViewAdapter()
 
-        */
+        // 레이아웃 매니저 등록
+        recyclerviewShare.layoutManager = LinearLayoutManager(activity)
+        // 리사이클러 뷰 어댑터 등록
+        recyclerviewShare.adapter = ShareViewAdapter()
+        recyclerviewShare.addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+
+
 
         // 나눔 게시글 추가(게시글 추가 액티비티로 이동)
         btnShareAdd.setOnClickListener {
@@ -84,9 +80,9 @@ class ShareFragment : Fragment() {
         return view
     }
 
-    /*22222222
     // 뷰 홀더
-    inner class ShareViewHolder(val binding: ItemSharelistBinding): RecyclerView.ViewHolder(binding.root)
+    //inner class ShareViewHolder(val binding: ItemSharelistBinding): RecyclerView.ViewHolder(binding.root)
+    inner class ShareViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     // 뷰 어댑터
     inner class ShareViewAdapter():
@@ -97,18 +93,30 @@ class ShareFragment : Fragment() {
         }
 
         // 뷰 홀더 준비
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            ShareViewHolder(ItemSharelistBinding.inflate(LayoutInflater.from(parent.context), parent,false))
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view =
+                LayoutInflater.from(parent.context).inflate(R.layout.item_sharelist, parent, false)
+            return ShareViewHolder(view)
+        }
 
         // 뷰 홀더의 뷰에 데이터 호출 (실제 데이터 출력)
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val binding = (holder as ShareViewHolder).binding
-            //뷰에 데이터 출력
-            binding.listTitle.text = postAllList!![position].title
-            binding.listLocation.text = postAllList!![position].location
-            binding.listName.text = postAllList!![position].name
-            binding.listDeadline.text = postAllList!![position].deadline
-            binding.listCreatedAt.text = postAllList!![position].createdAt
+            // 바인딩
+            val viewHolder = (holder as ShareViewHolder).itemView
+            var listTitle: TextView = viewHolder.findViewById(R.id.listTitle)
+            var listLocation: TextView = viewHolder.findViewById(R.id.listLocation)
+            var listName: TextView = viewHolder.findViewById(R.id.listName)
+            var listDeadline: TextView = viewHolder.findViewById(R.id.listDeadline)
+            var listCreatedAt: TextView = viewHolder.findViewById(R.id.listCreatedAt)
+            var listImage: ImageView = viewHolder.findViewById(R.id.listImage)
+
+            //뷰에 데이터 출력 (리사이클러 뷰 아이템 정보)
+            listTitle.text = postAllList!![position].title
+            listLocation.text = postAllList!![position].location
+            listName.text = postAllList!![position].name
+            listDeadline.text = postAllList!![position].deadline
+            listCreatedAt.text = postAllList!![position].createdAt
+            //listImage.drawable = ContextCompat.getDrawable(this, R.drawable.ic_noimg)
         }
     }
 
@@ -127,33 +135,9 @@ class ShareFragment : Fragment() {
                             }
                         }
                     }
-                    binding.recyclerviewShare.adapter?.notifyDataSetChanged()
+                    recyclerviewShare.adapter?.notifyDataSetChanged()
                 }
         }
     }
-
-    */
-
-    /* 민영쓰
-    // 뷰 어댑터
-    inner class ShareAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            var view = LayoutInflater.from(parent.context).inflate((R.layout.item_sharelist), parent, false)
-            return ViewHolder(view)
-        }
-
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
-        // view와 실제 데이터 연결
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-        }
-
-        override fun getItemCount(): Int {
-            return postAllList.size
-        }
-    }
-     */
 
 }
