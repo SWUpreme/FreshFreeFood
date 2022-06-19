8bc7f4faf7dfbb97ec7aad19744b64a2d4c1ad04
MinY-Lee <leemy08@naver.com>
2022-06-19 22:31:12 +0900

    [FIX] 삭제 버튼 UI수정

--- app/src/main/java/com/example/fffroject/ShareDetailActivity.kt
+++ app/src/main/java/com/example/fffroject/ShareDetailActivity.kt
@@ -3,6 +3,7 @@ package com.example.fffroject
 import android.graphics.BitmapFactory
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
