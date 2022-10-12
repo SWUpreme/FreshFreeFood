76ac243d5d10ae04ab82534fe4bc2a791ae1a8bf
MinY-Lee <leemy08@naver.com>
2022-10-12 16:26:44 +0900

    [ADD] EnvlevelFragment 환경 기여도 뱃지 추가 및 기여도에 따른 연동

--- app/src/main/java/com/example/fffroject/fragment/EnvlevelFragment.kt
+++ app/src/main/java/com/example/fffroject/fragment/EnvlevelFragment.kt
@@ -1,9 +1,11 @@
 package com.example.fffroject.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
@@ -22,6 +24,20 @@ class EnvlevelFragment: Fragment() {
     lateinit var text_envcontri: TextView
     // 환경 기여도 레벨
     var envpercent = 0
+    var envlevel = 0
+
+    // 이미지뷰 설정
+    lateinit var img_tree1: ImageView
+    lateinit var img_tree2: ImageView
+    lateinit var img_tree3: ImageView
+    lateinit var img_tree4: ImageView
+    lateinit var img_tree5: ImageView
+    lateinit var img_tree6: ImageView
+    lateinit var img_tree7: ImageView
+    lateinit var img_tree8: ImageView
+    lateinit var img_tree9: ImageView
+    lateinit var img_topbedge: ImageView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,13 +53,39 @@ class EnvlevelFragment: Fragment() {
         // 파이어스토어 인스턴스 초기화
         firestore = FirebaseFirestore.getInstance()
 
+        // 프로그래스바 부분 연결
         progress_envlevel = view.findViewById(R.id.progEnvLev)
         text_envcontri = view.findViewById(R.id.textEnvContri)
+
+        // 이미지뷰 부분(나무들) 연결
+        img_tree1 = view.findViewById(R.id.imgTree1)
+        img_tree2 = view.findViewById(R.id.imgTree2)
+        img_tree3 = view.findViewById(R.id.imgTree3)
+        img_tree4 = view.findViewById(R.id.imgTree4)
+        img_tree5 = view.findViewById(R.id.imgTree5)
+        img_tree6 = view.findViewById(R.id.imgTree6)
+        img_tree7 = view.findViewById(R.id.imgTree7)
+        img_tree8 = view.findViewById(R.id.imgTree8)
+        img_tree9 = view.findViewById(R.id.imgTree9)
+        img_topbedge = view.findViewById(R.id.imgTopBedge)
+
+        // 기본 색 #9F9F9F로 설정 (GRAY)
+        img_tree1.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree2.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree3.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree4.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree5.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree6.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree7.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree8.setColorFilter(Color.parseColor("#9F9F9F"))
+        img_tree9.setColorFilter(Color.parseColor("#9F9F9F"))
 
 
         // 파이어베이스에서 환경 기여도 가져와서 설정
         loadEnvLev()
 
+        // 환경 기여도에 따른 뱃지 보여주기
+        setTreeImage()
 
         return view
     }
@@ -63,6 +105,76 @@ class EnvlevelFragment: Fragment() {
                     }
                 }
 
+        }
+    }
+
+    fun setTreeImage() {
+        // 트리 이미지 세팅해주기 (먼저 envlevel부터 불러오기)
+        if (user != null) {
+            firestore?.collection("user")?.document(user!!.uid)
+                ?.get()?.addOnSuccessListener { document ->
+                    if (document != null) {
+                        envlevel = document?.data?.get("envlevel").toString().toInt()
+                        when(envlevel) {
+                            1 -> {
+                                img_tree1.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree1)}
+                            2 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree2)}
+                            3 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree3)}
+                            4 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree4)}
+                            5 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_tree5.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree5)}
+                            6 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_tree5.setColorFilter(null)
+                                img_tree6.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree6)}
+                            7 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_tree5.setColorFilter(null)
+                                img_tree6.setColorFilter(null)
+                                img_tree7.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree7)}
+                            8 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_tree5.setColorFilter(null)
+                                img_tree6.setColorFilter(null)
+                                img_tree7.setColorFilter(null)
+                                img_tree8.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree8)}
+                            9 -> {img_tree1.setColorFilter(null)
+                                img_tree2.setColorFilter(null)
+                                img_tree3.setColorFilter(null)
+                                img_tree4.setColorFilter(null)
+                                img_tree5.setColorFilter(null)
+                                img_tree6.setColorFilter(null)
+                                img_tree7.setColorFilter(null)
+                                img_tree8.setColorFilter(null)
+                                img_tree9.setColorFilter(null)
+                                img_topbedge.setImageResource(R.drawable.eco_tree9)}
+                            else -> { }
+                        }
+                    }
+                }
         }
     }
 }
