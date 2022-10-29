33093fa557cbe843a57e4a13c84b061dd797bf37
minji05 <smj95133@likelion.org>
2022-10-29 21:17:45 +0900

    [ADD] writeactivity 수정(2)

--- app/src/main/java/com/example/fffroject/WriteActivity.kt
+++ app/src/main/java/com/example/fffroject/WriteActivity.kt
@@ -18,7 +18,9 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_barcode.*
 import kotlinx.android.synthetic.main.activity_write.*
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -100,27 +102,58 @@ class WriteActivity : AppCompatActivity() {
 //            )
 
             if (checkAllWritten()) {
-                if (user != null) {
-
-                    var food_deadline =
-                        deadline_year.text.toString() + "." + deadline_month.text.toString() + "." + deadline_day.text.toString()
-                    var purchasedAt =
-                        purchasedAt_year.text.toString() + "." + purchasedAt_month.text.toString() + "." + purchasedAt_day.text.toString()
-                    foodindex = UUID.randomUUID().toString()
-                    firestore?.collection("fridge")?.document("$fridgeindex")
-                        ?.collection("food")?.document("$foodindex")
-                        ?.set(
-                            hashMapOf(
-                                "index" to foodindex,
-                                "name" to name.text.toString(),
-                                "deadline" to food_deadline,
-                                "purchaseAt" to purchasedAt,
-                                "count" to count.text.toString().toInt(),
-                                "done" to done
+                var formatter = SimpleDateFormat("yyyy.MM.dd")
+                var nowdate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+                var deadline = nowdate[0] + "." + nowdate[1] + "." + nowdate[2]
+                var date = formatter.parse(deadline).time
+                var day = formatter.parse(nowdate).time
+                var d_day = (date - day)/ (60 * 60 * 24 * 1000)
+                if (d_day.toInt() >= 0){
+                    if (user != null) {
+                        var food_deadline =
+                            deadline_year.text.toString() + "." + deadline_month.text.toString() + "." + deadline_day.text.toString()
+                        var purchasedAt =
+                            purchasedAt_year.text.toString() + "." + purchasedAt_month.text.toString() + "." + purchasedAt_day.text.toString()
+                        foodindex = UUID.randomUUID().toString()
+                        firestore?.collection("fridge")?.document("$fridgeindex")
+                            ?.collection("food")?.document("$foodindex")
+                            ?.set(
+                                hashMapOf(
+                                    "index" to foodindex,
+                                    "name" to name.text.toString(),
+                                    "deadline" to food_deadline,
+                                    "purchaseAt" to purchasedAt,
+                                    "count" to count.text.toString().toInt(),
+                                    "done" to done
+                                )
                             )
-                        )
 
+                    }
                 }
+                else {
+                    Toast.makeText(this, "유통기한이 이미 지난 제품입니다.", Toast.LENGTH_SHORT).show()
+                }
+//                if (user != null) {
+//
+//                    var food_deadline =
+//                        deadline_year.text.toString() + "." + deadline_month.text.toString() + "." + deadline_day.text.toString()
+//                    var purchasedAt =
+//                        purchasedAt_year.text.toString() + "." + purchasedAt_month.text.toString() + "." + purchasedAt_day.text.toString()
+//                    foodindex = UUID.randomUUID().toString()
+//                    firestore?.collection("fridge")?.document("$fridgeindex")
+//                        ?.collection("food")?.document("$foodindex")
+//                        ?.set(
+//                            hashMapOf(
+//                                "index" to foodindex,
+//                                "name" to name.text.toString(),
+//                                "deadline" to food_deadline,
+//                                "purchaseAt" to purchasedAt,
+//                                "count" to count.text.toString().toInt(),
+//                                "done" to done
+//                            )
+//                        )
+//
+//                }
             } else {
                 Toast.makeText(this, "내용을 입력하세요.", Toast.LENGTH_SHORT).show()
             }
