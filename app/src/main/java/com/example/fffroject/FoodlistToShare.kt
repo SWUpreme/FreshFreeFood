87a58b96d83c43e8bf37e5fde6e9c390e5e49c0e
Minjeong Kim <serak0310@naver.com>
2022-10-31 18:24:50 +0900

    [ADD] refriToShare 화면

---
+++ app/src/main/java/com/example/fffroject/FoodlistToShare.kt
@@ -0,0 +1,11 @@
+package com.example.fffroject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodlistToShare : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foodlist_to_share)
+    }
+}
