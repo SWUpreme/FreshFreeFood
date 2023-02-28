98616789c68f1f6fa4032316ff1fdebd91bb6212
Minjeong Kim <serak0310@naver.com>
2023-03-01 01:24:42 +0900

    trial : 나의 나눔 UI 변경 방법 : TabLayout과 ViewPager 사용 참고자료 : https://kangmin1012.tistory.com/12 결과 : 화면 X

--- app/src/main/java/com/example/fffroject/share/MySharePageAdapter.kt
+++ app/src/main/java/com/example/fffroject/share/MySharePageAdapter.kt
@@ -3,16 +3,18 @@ package com.example.fffroject.share
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.fffroject.fragment.MyshareIngFragment
+
+import com.example.fffroject.shareTrial.MyShareFragmentTab
+import java.util.concurrent.CountDownLatch
 
 class MySharePageAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm){
-    private var fragments : ArrayList<Fragment> = ArrayList()
+    private var fragments : ArrayList<MyShareFragmentTab> = ArrayList()
 
     override fun getItem(position: Int): Fragment = fragments[position]
 
     override fun getCount(): Int = fragments.size
 
-    fun addItems(fragment : MyshareIngFragment){
+    fun addItems(fragment : MyShareFragmentTab){
         fragments.add(fragment)
     }
 }
--- app/src/main/java/com/example/fffroject/shareTrial/MyShareFragmentTab.kt
+++ app/src/main/java/com/example/fffroject/shareTrial/MyShareFragmentTab.kt
@@ -1,4 +1,5 @@
-package com.example.fffroject.fragment
+package com.example.fffroject.shareTrial
+
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,19 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fffroject.R
-import kotlinx.android.synthetic.main.fragment_myshare_ing.view.*
+import kotlinx.android.synthetic.main.fragment_my_share_tab.view.*
 
 
-class MyshareIngFragment : Fragment() {
+class MyShareFragmentTab : Fragment() {
     var name = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view =inflater.inflate(R.layout.fragment_myshare_ing, container, false)
+        val view =inflater.inflate(R.layout.tab_myshare_button, container, false)
         view.textView.text = name
 
         return view
     }
+
+
 }
