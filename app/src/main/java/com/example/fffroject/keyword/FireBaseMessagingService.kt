847d9b812c0d2487ac61dea4ecbbbdf618415a7d
serak0310 <serak0310@naver.com>
2023-06-16 11:41:20 +0900

    파이어베이스 쪽지 알림 메시징 전송

--- app/src/main/java/com/example/fffroject/keyword/FireBaseMessagingService.kt
+++ app/src/main/java/com/example/fffroject/keyword/FireBaseMessagingService.kt
@@ -74,10 +74,10 @@ class FireBaseMessagingService : FirebaseMessagingService() {
         val takerId = remoteMessage.data["taker"]
         val chatSenderId = remoteMessage.data["chatSenderId"]
         val chatSenderNickname = remoteMessage.data["chatSenderNickname"]
-//        var title = remoteMessage.data["title"]
-//        var chatContent = remoteMessage.data["body"]
-        var title = remoteMessage.notification!!.title
-        var chatContent = remoteMessage.notification!!.body
+        var title = remoteMessage.data["title"]
+        var chatContent = remoteMessage.data["body"]
+//        var title = remoteMessage.notification!!.title
+//        var chatContent = remoteMessage.notification!!.body
 
         Log.d("chatroomId 받아오는지:", "${chatroomId}")
         Log.d("postId 받아오는지:", "${postId}")
